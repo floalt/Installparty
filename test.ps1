a8d64676bf702eef0f7c0398669f41500988e686
floalt <flo.alt@fa-netz.de>
2020-05-02 22:06:33 +0200

    adding new file

--- test.ps1
+++
@@ -1,7 +0,0 @@
-﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{ Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-
-$policy = Get-ExecutionPolicy
-echo "ExecutionPolicy is $policy"
-cp C:\installparty\test.ps1 c:\
-pause
